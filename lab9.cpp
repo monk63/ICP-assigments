a13887fcd210df0144ffaf72881482c320fec8ec
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-03-31 16:47:29 +0000

    Update lab9.cpp

--- lab9.cpp
+++ lab9.cpp
@@ -5,42 +5,10 @@
 using namespace std;
 
 
-/*void populateArray(){
-
-    int i,j;
-    int A[101][101];
-int **ptr = new int *[i];
-      for(int i = 0; i<101; i++)
-    {
-        ptr[i] = new int[101];
-    }
-    srand(time(0));
-
-    for(int j = 0; j<101; j++)
-    {
-        for(int k = 0; k<101; k++)
-        {
-            int a = rand()%10;
-            ptr[j][k] = a;
-            cout<<ptr[j][k]<<" ";
-        }
-    
-        cout<<endl; 
-    }   
-
-}
-
-*/
 int main(){
     int i,j;
     int A[101][101];
-   // int count=0;
-
-    // populateArray();
-      //return 0;
-    
-
-  //
+ 
  int populateArray(int i);
 int **ptr = new int *[i];
       for(int i = 0; i<101; i++)
