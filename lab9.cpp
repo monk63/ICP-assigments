88c6888715a902873512ecfc36f19f17e513f6db
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-04-01 15:06:49 +0000

    Update lab9.cpp

--- lab9.cpp
+++ lab9.cpp
@@ -1,38 +1,102 @@
-#include <iostream>
-#include<iomanip>
-#include <ctime> // For time()
-#include <cstdlib>  // For srand() and rand() 
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#define MAXX 101
+#define endl "\n"
+#define fastIo                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
 using namespace std;
 
-
-int main(){
-    int i,j;
-    int A[101][101];
- 
- int populateArray(int i);
-int **ptr = new int *[i];
-      for(int i = 0; i<101; i++)
+int A[MAXX][MAXX];
+void seachValidEntries(int xl, int yl, int xm, int ym)
+{
+    int count = 0;
+    for (int i = xl; i < xm; i++)
     {
-        ptr[i] = new int[101];
-    }
-    srand(time(0));
-
-    for(int j = 0; j<101; j++)
-    {
-        for(int k = 0; k<101; k++)
+        for (int j = yl; j < ym; j++)
         {
-            // rand()%10 restricts the random number to a range of 0-10.
-            int a = rand()%10;
-            ptr[j][k] = a;
-            cout<<ptr[j][k]<<" ";
+            if (A[i][j] >= 1)
+
+            {
+                count++;
+                printf("%2d, %2d; %2d \n", i, j, A[i][j]);
+            }
         }
-
-  /*  int searchValidEntries()  
-        
-    }  
-    */
-        cout<<endl; 
-    }   
-
-    
+    }
+    cout << "\nTotal entries in Bounded Box:" << ((xm - xl) * (ym - yl)) << endl;
+    cout << "Total valid Entries: " << count << endl
+         << endl;
 }
+void populateArray(int N)
+{
+    srand((unsigned)time(NULL));
+    for (int i{}; i < N; i++)
+    {
+        int p = 0 + (rand() % 50);
+        int q = 0 + (rand() % 50);
+        if (A[p][q] == -1)
+        {
+            A[p][q] = 1;
+        }
+        else if (A[p][q] >= 1)
+        {
+            A[p][q] += 1;
+        }
+        // cout << p << " " << q << endl;
+    }
+}
+void printArray(int xl, int yl, int xm, int ym)
+{
+    for (int i = xl; i < xm; i++)
+    {
+        for (int j = yl; j < ym; j++)
+        {
+            printf("%3d ", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    //fastIo;
+    memset(A, -1, sizeof(A));
+    int N = 2000;
+    populateArray(N);
+    int n;
+    cout << "Input the no. of Bounded Boxes:";
+    cin >> n;
+    while (n--)
+    {
+        int xl, xm, yl, ym;
+        //cout << "Input  Lower X axis:";
+        cin >> xl;
+        //cout << "Input  Lower Y axis:";
+        cin >> yl;
+        //cout << "Input Higher X axis:";
+        cin >> xm;
+        //cout << "Input Higher Y axis:";
+        cin >> ym;
+        if (xm < xl || ym < yl)
+        {
+            cout << "This box can't be possible" << endl;
+            break;
+            return 0;
+        }
+        //! uncomment it to see what is bounded box looking now
+         printArray(xl, yl, xm, ym);
+          cout<<endl;
+        seachValidEntries(xl, yl, xm, ym);
+    }
+    return 0;
+}
+//input
+
+// 5
+// 80 1 100 10
+// 10 20 40 79
+// 1 90 100 100
+// 40 30 70 60
+// 50 50 70 70
+//
